4fcc015daa9b00a4edeca962b158dcfd18a3722b
Brandon Amos <brandon.amos.cs@gmail.com>
2021-08-09 11:51:31 -0400

    fix spaces

--- generate.py
+++ generate.py
@@ -507,7 +507,7 @@ MARKDOWN_CONTEXT = RenderContext(
     ),
     [
         (r'\\\\\[[^\]]*]', '\n'),  # newlines
-        # (r'~', ' '),  # spaces
+        (r'~', ' '),  # spaces
         (r'\\ ', ' '),  # spaces
         (r'\\&', '&'),  # unescape &
         (r'\\\$', '\$'),  # unescape $
