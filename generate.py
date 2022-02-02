31872fc355ba59f1e939359620a13a786b348085
natolambert <nol@berkeley.edu>
2022-02-02 14:27:40 -0800

    is now my cv

--- generate.py
+++ generate.py
@@ -483,7 +483,7 @@ class RenderContext(object):
                 section_template_name = os.path.join(
                     self.SECTIONS_DIR, 'industry' + self._file_ending)
             elif section_tag in ['coursework', 'education', 'honors',
-                                 'industry', 'research', 'skills', 'service',
+                                 'industry', 'research', 'skills', 'service', 'reviewing',
                                  'teaching', 'talks', 'advising']:
                 section_data['items'] = section_content
                 section_template_name = os.path.join(
